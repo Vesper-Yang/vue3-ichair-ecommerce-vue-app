c786adc770468aaaab0c0da53d543ed6c09d3363
Vesper-Yang <1105061841@qq.com>
2024-09-05 14:45:02 +0800

    当网页滚动导航栏Header不见时-购物车悬浮在网站右侧

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,13 @@
   <main id="id">
     <RouterView />
   </main>
+  <CartBtnLoader />
 </template>
 
 <script setup>
-  import Header from './components/Header.vue';
-  import SideBar from './components/SideBar.vue';
+import CartBtnLoader from './components/CartBtnLoader.vue';
+import Header from './components/Header.vue';
+import SideBar from './components/SideBar.vue';
 </script>
 
 <style scoped>
